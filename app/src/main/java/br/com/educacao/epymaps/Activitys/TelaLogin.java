8237dc022e36fc32d5fe68030a9b3134aa79557a
Djair <jairmonteiromdm@gmail.com>
2017-11-01 16:47:39 -0200

    integracao com o firebase realizado

--- app/src/main/java/br/com/educacao/epymaps/Activitys/TelaLogin.java
+++ app/src/main/java/br/com/educacao/epymaps/Activitys/TelaLogin.java
@@ -4,10 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import br.com.educacao.epymaps.DAO.UsuarioDAO;
 import br.com.educacao.epymaps.R;
@@ -25,6 +33,10 @@ public class TelaLogin extends AppCompatActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // inserido para comunicar com o facebook
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.tela_login);
 
         btnNovaConta = (Button) findViewById(R.id.btnNovaConta);
@@ -35,6 +47,22 @@ public class TelaLogin extends AppCompatActivity {
         btnRecuperarSenha = (Button) findViewById(R.id.btnRecupSenha);
         edtEmail.getBackground().setAlpha(60);
         edtSenha.getBackground().setAlpha(60);
+
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         usuarioDAO = new UsuarioDAO(TelaLogin.this);
 
@@ -87,7 +115,13 @@ public class TelaLogin extends AppCompatActivity {
 
         return true;
     }
-}
+
+
+
+
+    }
+
+
 
 
 /*
